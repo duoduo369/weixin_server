1d80ade543a194970c2bbe6cba493a12e845cc80
duoduo369 <duoduo3369@gmail.com>
2016-04-11 15:00:21 +0800

    add handler example

--- weixin_server/weixin_server/views.py
+++ weixin_server/weixin_server/views.py
@@ -4,10 +4,11 @@ from django.views.generic import View
 from django.http.response import HttpResponse
 from weixin.utils import wechat
 from wechat_sdk.exceptions import ParseError
+from .mixins import WeixinDispatchMixin
 
 log = logging.getLogger(__name__)
 
-class IndexView(View):
+class IndexView(View, WeixinDispatchMixin):
 
     def get(self, request):
         signature = request.GET.get('signature', '')
@@ -18,19 +19,13 @@ class IndexView(View):
             return HttpResponse(echostr)
         return HttpResponse('signature error')
 
-    def post(self, request):
-        content = request.body
-        signature = request.GET.get('signature', '')
-        msg_signature = request.GET.get('msg_signature', '')
-        timestamp = request.GET.get('timestamp', '')
-        nonce = request.GET.get('nonce', '')
-        try:
-            wechat.parse_data(
-                    content,
-                    msg_signature=msg_signature,
-                    timestamp=timestamp,
-                    nonce=nonce)
-        except ParseError:
-            return HttpResponse('Invalid Body Text')
-        response_xml = wechat.response_text(content='get: {}'.format(wechat.message.content))
+    def post(self, request, *args, **kwargs):
+        return self.dispatch_weixin(request, *args, **kwargs)
+
+    def weixin_handler_text(self, request, parsed_wechat, *args, **kwargs):
+        response_xml = parsed_wechat.response_text(content=u'get: {}'.format(wechat.message.content))
+        return HttpResponse(response_xml, content_type='application/xml')
+
+    def weixin_handler_image(self, request, parsed_wechat, *args, **kwargs):
+        response_xml = wechat.response_image(media_id=parsed_wechat.message.media_id)
         return HttpResponse(response_xml, content_type='application/xml')
