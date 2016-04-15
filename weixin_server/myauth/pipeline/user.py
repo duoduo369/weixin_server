7cf63d934379a4918f7739c1ae74095ebc1b250f
duoduo369 <duoduo3369@gmail.com>
2016-04-15 14:45:35 +0800

    fix profile data

--- weixin_server/myauth/pipeline/user.py
+++ weixin_server/myauth/pipeline/user.py
@@ -5,15 +5,24 @@ USER_FIELDS = ['username', 'email']
 
 def save_profile(backend, user, response, *args, **kwargs):
     '''只有第一次的时候更新'''
+    need_save = False
+    details = kwargs.get('details', {})
     try:
         profile = user.profile
     except UserProfile.DoesNotExist:
         profile = UserProfile()
         profile.user = user
-        details = kwargs.get('details', {})
         profile.name = details.get('username') or profile.name or ''
+        need_save = True
+    if not profile.avatar:
         profile.avatar = details.get('profile_image_url', '')
-        if backend.name.startswith('weixin'):
-            profile.weixin_unionid = response.get('unionid', '')
+        if profile.avatar:
+            need_save = True
+    if backend.name.startswith('weixin'):
+        unionid = response.get('unionid', '')
+        if unionid and not profile.weixin_unionid:
+            profile.weixin_unionid = unionid
+            need_save = True
+    if need_save:
         profile.save()
     return {'profile': profile}
